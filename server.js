fbee51bd3c4e277b66ca1f320e452a02a502395b
nnome7022 <nnome7022@gmail.com>
2025-06-04 02:14:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const fs = require('fs') // Required for reading the certificate and key files
-let options = { key: fs.readFileSync("/opt/intergram/key.pem"), cert: fs.readFileSync("/opt/intergram/bundle.crt") };
-
 const request = require('request');
 const compression = require('compression');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const http = require ('https').Server(options,app);
+const http = require ('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('dist', {index: 'demo.html', maxage: '4h'}));
@@ -92,8 +89,8 @@ app.post('/usage-end', cors(), function(req, res) {
     res.end();
 });
 
-http.listen(process.env.PORT || 8443, function(){
-    console.log('listening on port:' + (process.env.PORT || 8443));
+http.listen(process.env.PORT || 3001, function(){
+    console.log('listening on port:' + (process.env.PORT || 3001));
     console.log(process.env.TELEGRAM_TOKEN);
 });
 
